12749359554bd8750a6f3f30fb3f50c93d73a28c
Alexandru Tenche <alex.tenche@gmail.com>
2017-07-24 18:51:08 +0300

    first commit

---
+++ read_csv.py
@@ -0,0 +1,15 @@
+import csv
+import numpy
+
+def open_with_csv(filename, d='\t'):
+    data = []
+
+    with open(filename, encoding='utf-8') as tsvin:
+        tie_reader = csv.reader(tsvin, delimiter=d)
+        for line in tie_reader:
+            data.append(line)
+    return data
+
+
+data_from_csv = open_with_csv('data.csv')
+print(data_from_csv[0])
