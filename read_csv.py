1cfc08b5f5742f6603b246e65939c998e46a858f
Alexandru Tenche <alex.tenche@gmail.com>
2017-07-24 23:56:33 +0300

    added consts

--- read_csv.py
+++ read_csv.py
@@ -1,6 +1,13 @@
 import csv
 import numpy
 
+FIELDNAMES = ['', 'id', 'priceLabel', 'name', 'brandId', 'brandName', 'imageLink', 'desc', 'vendor', 'print',
+              'material']
+DATATYPES = [('myint', 'i'), ('myid', 'i'), ('price', 'f8'), ('names', 'a200'), ('brandId', '<i8'),
+             ('brandName', 'a200'), ('imageLink', '|S500'), ('description', '|S900'), ('vendor', '|S100'),
+             ('pattern', '|S50'), ('material', '|S50')]
+
+
 def open_with_csv(filename, d='\t'):
     data = []
 
@@ -11,5 +18,19 @@ def open_with_csv(filename, d='\t'):
     return data
 
 
+def load_data(filename, d='\t'):
+    my_csv = numpy.genfromtxt(
+        filename,
+        delimiter=d,
+        skip_header=1,
+        invalid_raise=False,
+        names=FIELDNAMES,
+        dtype=DATATYPES
+    )
+    return my_csv
+
+
 data_from_csv = open_with_csv('data.csv')
-print(data_from_csv[0])
+# print(data_from_csv[0])
+
+my_csv = load_data('data.csv')
