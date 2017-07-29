be6393ec19af01d574c2e2eb4612da4448bbeed0
Alexandru Tenche <alex.tenche@gmail.com>
2017-07-29 19:38:30 +0200

    started working with pandas

---
+++ pandas/basics.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use('ggplot')
+
+web_stats = {
+    'Day' : [1, 2, 3, 4, 5, 6],
+    'Visitors': [12, 23, 22, 41, 11, 55],
+    'test' : [1, 1 ,2 ,3, 5, 6]
+}
+
+
+
