4e3745062bbab8380633f4d8c457c9353b3e23ef
Alexandru Tenche <alex.tenche@gmail.com>
2017-07-30 15:33:11 +0200

    create dataframe from dict

--- pandas/basics.py
+++ pandas/basics.py
@@ -1,13 +1,10 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-from matplotlib import style
-style.use('ggplot')
 
 web_stats = {
-    'Day' : [1, 2, 3, 4, 5, 6],
+    'Day': [1, 2, 3, 4, 5, 6],
     'Visitors': [12, 23, 22, 41, 11, 55],
-    'test' : [1, 1 ,2 ,3, 5, 6]
+    'Bounce_Rate': [55, 45, 65, 60, 70, 66]
 }
 
-
-
+df = pd.DataFrame(web_stats)
+print(df)
